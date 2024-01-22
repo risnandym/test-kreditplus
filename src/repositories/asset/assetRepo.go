fba87fd71b78cbe6e26b33ef247f04e815c96f5e
risnandym <risnandy.maulana@visionplus.id>
2024-01-22 22:35:26 +0700

    clean

--- src/repositories/asset/assetRepo.go
+++ src/repositories/asset/assetRepo.go
@@ -1,6 +1,7 @@
 package asset_repo
 
 import (
+	"log"
 	"test-kreditplus/src/entities"
 	"time"
 
@@ -26,6 +27,7 @@ func (a AssetRepository) Create(db *gorm.DB, request *entities.Asset) (response 
 	request.CreatedAt = time.Now()
 	request.UpdatedAt = time.Now()
 	if err = a.db.Create(request).Error; err != nil {
+		log.Printf("asset create error: %v", err)
 		return
 	}
 
@@ -45,6 +47,7 @@ func (a AssetRepository) CreateInBatches(db *gorm.DB, request []*entities.Asset)
 	}
 
 	if err = a.db.CreateInBatches(request, len(request)).Error; err != nil {
+		log.Printf("asset create in batches error: %v", err)
 		return
 	}
 
@@ -60,6 +63,7 @@ func (a AssetRepository) Update(db *gorm.DB, request *entities.Asset) (response 
 
 	request.UpdatedAt = time.Now()
 	if err = a.db.Save(request).Error; err != nil {
+		log.Printf("asset Update error: %v", err)
 		return
 	}
 
@@ -70,6 +74,7 @@ func (a AssetRepository) Update(db *gorm.DB, request *entities.Asset) (response 
 func (a AssetRepository) Get(id uint) (response *entities.Asset, err error) {
 
 	if err = a.db.Where("id = ?", id).First(response).Error; err != nil {
+		log.Printf("asset get error: %v", err)
 		return
 	}
 
